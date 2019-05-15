327c003702d90bc0144a088d66d1acf3428bb585
desktopaware <fafalhumaid@gmail.com>
2019-05-15 14:49:10 +0300

    updates....

--- app/src/main/java/android/example/bakingapp/BakingAdapter.java
+++ app/src/main/java/android/example/bakingapp/BakingAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -37,7 +38,7 @@ public class BakingAdapter extends RecyclerView.Adapter<BakingAdapter.ItemViewHo
 
         itemViewHolder.recipeName.setText(recipe.get(i).getName());
 
-        if(recipe.get(i).getImage().equals("")){
+        if(TextUtils.isEmpty(recipe.get(i).getImage())){
 
             itemViewHolder.imageView.setImageResource(R.drawable.baking_image);
         }else {
--- app/src/main/java/android/example/bakingapp/StepsInformationFragment.java
+++ app/src/main/java/android/example/bakingapp/StepsInformationFragment.java
@@ -7,10 +7,12 @@ import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.exoplayer2.ExoPlayerFactory;
@@ -21,6 +23,7 @@ import com.google.android.exoplayer2.ui.PlayerView;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,10 +33,12 @@ public class StepsInformationFragment extends Fragment {
     private TextView noVideo, desctriptionTv;
     private PlayerView playerView;
     private SimpleExoPlayer exoPlayer;
-    private Button next, back;
+    private Button next, previous;
     private List<Steps> steps;
-    private int position, nextPosition, backPosition;
+    private int position, nextPosition, previousPosition;
     private boolean mTwoPane;
+    private ImageView thumbnail;
+    int currentWindow;
     boolean playerStatus;
     long playerPosition;
     String url;
@@ -62,53 +67,48 @@ public class StepsInformationFragment extends Fragment {
         noVideo = view.findViewById(R.id.noVideo);
         desctriptionTv = view.findViewById(R.id.description_step_tv);
         next = view.findViewById(R.id.button_next);
-        back = view.findViewById(R.id.button_back);
+        previous = view.findViewById(R.id.button_back);
         playerView = view.findViewById(R.id.exo_play);
+        thumbnail = view.findViewById(R.id.thumbnail);
 
         steps = getArguments().getParcelableArrayList("steps");
         position = getArguments().getInt("position");
         mTwoPane = getArguments().getBoolean("mTwoPane");
 
         nextPosition = position + 1;
-        backPosition = position - 1;
+        previousPosition = position - 1;
 
         desctriptionTv.setText(steps.get(position).getDescription());
 
         Steps step = steps.get(position);
 
         if(position == 0 || mTwoPane){
-            back.setVisibility(View.GONE);
+            previous.setVisibility(View.GONE);
         }else {
-            back.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(getActivity(), StepsDetails.class);
-                    intent.putParcelableArrayListExtra("steps", (ArrayList<? extends Parcelable>) steps);
-                    intent.putExtra("position", backPosition);
-                    startActivity(intent);
-                    getActivity().finish();
+            previous.setOnClickListener(view1 -> {
+                Intent intent = new Intent(getActivity(), StepsDetails.class);
+                intent.putParcelableArrayListExtra("steps", (ArrayList<? extends Parcelable>) steps);
+                intent.putExtra("position", previousPosition);
+                startActivity(intent);
+                getActivity().finish();
 
-                }
             });
         }
 
         if(position == steps.size()-1 || mTwoPane){
             next.setVisibility(View.GONE);
         }else {
-            next.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(getActivity(), StepsDetails.class);
-                    intent.putParcelableArrayListExtra("steps", (ArrayList<? extends Parcelable>) steps);
-                    intent.putExtra("position", nextPosition);
-                    startActivity(intent);
-                    getActivity().finish();
+            next.setOnClickListener(view12 -> {
+                Intent intent = new Intent(getActivity(), StepsDetails.class);
+                intent.putParcelableArrayListExtra("steps", (ArrayList<? extends Parcelable>) steps);
+                intent.putExtra("position", nextPosition);
+                startActivity(intent);
+                getActivity().finish();
 
-                }
             });
         }
 
-        if(step.getVideoURL().equals("")){
+        if(TextUtils.isEmpty(step.getVideoURL())){
 
             playerView.setVisibility(View.GONE);
 
@@ -133,6 +133,12 @@ public class StepsInformationFragment extends Fragment {
 
         }
 
+        if(!TextUtils.isEmpty(step.getThumbnailURL())){
+            Picasso.get()
+                    .load(step.getThumbnailURL())
+                    .into(thumbnail);
+        }
+
 
         return view;
     }
@@ -145,8 +151,19 @@ public class StepsInformationFragment extends Fragment {
 
         super.onSaveInstanceState(outState);
     }
+    private void releasePlayer() {
+        if (exoPlayer != null) {
+            // save the player state before releasing its resources
+            playerPosition = exoPlayer.getCurrentPosition();
+            currentWindow = exoPlayer.getCurrentWindowIndex();
+            playerStatus = exoPlayer.getPlayWhenReady();
+            exoPlayer.release();
+            exoPlayer = null;
+        }
+    }
 
-    @Override
+
+        @Override
     public void onDestroy() {
         super.onDestroy();
         if(exoPlayer != null){
@@ -156,22 +173,18 @@ public class StepsInformationFragment extends Fragment {
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
-
-    @Override
     public void onPause() {
         super.onPause();
+        if (Util.SDK_INT <= 23) {
+            releasePlayer();
+        }
     }
 
     @Override
     public void onStop() {
         super.onStop();
+        if (Util.SDK_INT > 23) {
+            releasePlayer();
+        }
     }
 }
